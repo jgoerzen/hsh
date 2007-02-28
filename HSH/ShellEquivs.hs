5cf56f62a767b9cfde666f89cea8e6801bb04071
John Goerzen <jgoerzen@complete.org>
2007-02-28 08:16:33 +0100

    ShellEquivs working, but still some trouble with runS

---
+++ HSH/ShellEquivs.hs
@@ -0,0 +1,112 @@
+{- Shell Equivalents
+
+Copyright (C) 2007 John Goerzen <jgoerzen@complete.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+-}
+
+{- |
+   Module     : HSH.ShellEquivs
+   Copyright  : Copyright (C) 2007 John Goerzen
+   License    : GNU GPL, version 2 or above
+
+   Maintainer : John Goerzen <jgoerzen@complete.org> 
+   Stability  : provisional
+   Portability: portable
+
+Copyright (c) 2006-2007 John Goerzen, jgoerzen\@complete.org
+-}
+
+module HSH.ShellEquivs(
+                       catFrom,
+                       catTo,
+                       grep,
+                       grepV,
+                       egrep,
+                       egrepV,
+                       tee,
+                       wcL,
+                      ) where
+
+import HSH.Command
+import Data.List
+import Text.Regex
+import Control.Monad
+
+{- | Load the specified files and display them, one at a time. 
+
+The special file @-@ means to display the input.
+
+If it is not given, no input is read.
+
+Unlike the shell cat, @-@ may be given twice.  However, if it is, you
+will be forcing Haskell to buffer the input. 
+
+Note: buffering behavior here is untested. 
+-}
+catFrom :: [FilePath] -> String -> IO String
+catFrom fplist inp =
+    foldM foldfunc "" fplist
+        where foldfunc accum fp =
+                  case fp of
+                    "-" -> return (accum ++ inp)
+                    fn -> do c <- readFile fn
+                             return (accum ++ c)
+
+{- | Takes input, writes it to the specified file, and does not pass it on. 
+     See also 'tee'. -}
+catTo :: FilePath -> String -> IO String
+catTo fp inp =
+    do writeFile fp inp
+       return ""
+
+{- | Takes input, writes it to all the specified files, and passes it on.
+
+This function buffers the input. 
+
+See also 'catFrom'. -}
+tee :: [FilePath] -> String -> IO String
+tee [] inp = return inp
+tee (x:xs) inp = do writeFile x inp
+                    tee xs inp
+
+{- | Search for the string in the lines.  Return those that match. -}
+grep :: String -> [String] -> [String]
+grep needle = filter (isInfixOf needle)
+
+{- | Search for the string in the lines.  Return those that do NOT match. -}
+grepV :: String -> [String] -> [String]
+grepV needle = filter (not . isInfixOf needle)
+
+{- | Search for the regexp in the lines.  Return those that match. -}
+egrep :: String -> [String] -> [String]
+egrep pat = filter (ismatch regex)
+    where regex = mkRegex pat
+          ismatch r inp = case matchRegex r inp of
+                            Nothing -> False
+                            Just _ -> True
+
+{- | Search for the regexp in the lines.  Return those that do NOT match. -}
+egrepV :: String -> [String] -> [String]
+egrepV pat = filter (not . ismatch regex)
+    where regex = mkRegex pat
+          ismatch r inp = case matchRegex r inp of
+                            Nothing -> False
+                            Just _ -> True
+
+{- | Count number of lines.  wc -l -}
+wcL :: [String] -> [String]
+wcL inp = [show $  genericLength inp]
+
