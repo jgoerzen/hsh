99dc54e7ad006babef0ccb1a4ced1b9e0277a5b0
John Goerzen <jgoerzen@complete.org>
2007-03-01 03:38:19 +0100

    test updates

--- testsrc/Core.hs
+++ testsrc/Core.hs
@@ -6,7 +6,9 @@ Please see the COPYRIGHT file
 module Core(tests) where
 import Test.HUnit
 import HSH
+import HSH.ShellEquivs
 import TestUtils
+import Data.Char
 
 basics =
     [cmdcase "echo" "hi\n" $ "echo hi",
@@ -17,15 +19,32 @@ basics =
                  ("wc", ["-l", "testsrc/testdata/quux"])
     ]
 
-basicpipe =
+pipes =
     [cmdcase "sh2sh" "100\n" $ "cat testsrc/testdata/quux" -|- "wc -l",
      cmdcase "sh2sh2sh" "14\n" $
              "cat testsrc/testdata/quux" -|- "grep oo" -|- "wc -l",
      cmdcase "sh2sh2sh2sh" "0000000 032061 000012\n0000003\n" $
-             "cat testsrc/testdata/quux" -|- "grep oo" -|- "wc -l" -|- "od"
+             "cat testsrc/testdata/quux" -|- "grep oo" -|- "wc -l" -|- "od",
+     cmdcase "ls baseline" lsbase lscmd,
+     cmdcase "sh|s->s" (map toUpper lsbase) $ lscmd -|- (map toUpper),
+     cmdcase "sh|s->s|sh" "BAR\nBAZ\n" $ 
+             lscmd -|- (map toUpper) -|- "grep BA",
+     cmdcase "sh|s->s|s->s" "BAR\nBAZ\n" $
+             lscmd -|- (map toUpper) -|- grep "BA",
+     cmdcase "s->s|sh" (map toUpper lsbase) $ catFromS lsbase -|- "tr a-z A-Z",
+     cmdcase "s->s|sh|sh" "BAR\nBAZ\n" $
+             catFromS lsbase -|- "tr a-z A-Z" -|- "grep BA",
+     cmdcase "s->s|sh|s->s" "BAR\nBAZ\n" $
+             catFromS lsbase -|- "tr a-z A-Z" -|- grep "BA",
+     cmdcase "s->s|s->s|sh" "BAR\nBAZ\n" $
+             catFromS lsbase -|- (map toUpper) -|- "grep BA",
+     cmdcase "s->s|s->s|s->s" "BAR\nBAZ\n" $
+             catFromS lsbase -|- (map toUpper) -|- grep "BA"
     ]
+    where lsbase = "bar\nbaz\nfoo\nquux\n"
+          lscmd = "ls testsrc/testdata"
        
 tests = TestList
         [TestLabel "basics" $ TestList basics,
-         TestLabel "basicpipe" $ TestList basicpipe]
+         TestLabel "pipes" $ TestList pipes]
 
